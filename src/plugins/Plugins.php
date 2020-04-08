50a71d3b6c6b22867ae29fd9b6b16fd7926047b3
diazoxide <aaron.yor@gmail.com>
2020-04-08 19:43:17 +0400

    Add plugin info bar

--- src/plugins/Plugins.php
+++ src/plugins/Plugins.php
@@ -18,6 +18,11 @@ class Plugins
 
     private $plugins;
 
+    public const STATUS_SYSTEM_DEFAULT = 0;
+    public const STATUS_FORCE_DISABLED = -1;
+    public const STATUS_FORCE_ENABLED = 1;
+    public const STATUS_SMART = 2;
+
     /**
      * @return string
      */
@@ -47,7 +52,26 @@ class Plugins
                 10,
                 3
             );
+
+            add_filter(
+                'wp-lib-option/' . $this->getName() . '/form-before-nested-fields',
+                [$this, 'setBeforeNestedFields'],
+                10,
+                3
+            );
             $this->settings[$file] = [
+                'status' => new Option(
+                    [
+                        'default' => self::STATUS_SYSTEM_DEFAULT,
+                        'label' => 'Status',
+                        'values' => [
+                            self::STATUS_SYSTEM_DEFAULT => 'System default',
+                            self::STATUS_FORCE_DISABLED => 'Force disabled',
+                            self::STATUS_FORCE_ENABLED => 'Force enabled',
+                            self::STATUS_SMART => 'Smart control',
+                        ]
+                    ]
+                ),
                 'require' => new Option(
                     [
                         'default' => [],
@@ -64,13 +88,81 @@ class Plugins
         }
     }
 
+    /**
+     * @param $label
+     * @param $route
+     * @param $parent
+     * @return string
+     */
     public function setNestedFieldName($label, $route, $parent): string
     {
-        $label = $this->plugins[$route]['Name'] ?? $label;
+        $plugin = $label;
+        $label = $this->plugins[$plugin]['Name'] ?? $label;
         $label .= ' (';
-        $label .= $this->plugins[$route]['Version'] ?? '?';
-        $label .= ')';
+        $label .= $this->plugins[$plugin]['Version'] ?? '?';
+        $label .= ') ';
+        $label .= $this->getPluginActiveStatusBadge($plugin, false);
         return $label;
+    }
+
+    /**
+     * @param $content
+     * @param $route
+     * @param $parent
+     * @return string
+     */
+    public function setBeforeNestedFields($content, $route, $parent): string
+    {
+        $html = '';
+        $plugin_data = $this->plugins[$route] ?? null;
+        if ($plugin_data) {
+            $html .= $this->getPluginActions($route);
+            $html .= $this->getPluginInfo($plugin_data);
+        }
+
+        $content = $html . $content;
+        return $content;
+    }
+
+    private function getPluginActions(string $plugin):string{
+        $html = '<div class="row actions">';
+        $html .= '<a class="button button-primary"></a>';
+        $html .= $this->getPluginActiveStatusBadge($plugin);
+        $html .='</div>';
+        return $html;
+    }
+
+    private function getPluginActiveStatusBadge(string $plugin, bool $with_label = true): string
+    {
+        $class = 'plugin-active-status-badge';
+        if (is_plugin_active($plugin)) {
+            $label = __('Activated', 'wordpress');
+            $class .= ' activated';
+        } else {
+            $label = __('Deactivated', 'wordpress');
+            $class .= ' deactivated';
+        }
+        return sprintf('<span class="%s">%s</span>', $class, $with_label ? $label : '');
+    }
+
+    /**
+     * @param array $plugin_data
+     * @return string
+     */
+    private function getPluginInfo(array $plugin_data): string
+    {
+        $table = '<table class="widefat fixed">';
+        $i = 0;
+        foreach ($plugin_data as $key => $value) {
+            if (!empty($value)) {
+                $alternate = ($i % 2) ? 'alternate' : '';
+                $table .= sprintf('<tr class="%s"><td>%s</td><td>%s</td></tr>', $alternate, $key, $value);
+                $i++;
+            }
+        }
+        $table .= '</table>';
+
+        return sprintf('<div class="plugin-data">%s</div>', $table);
     }
 
     /**

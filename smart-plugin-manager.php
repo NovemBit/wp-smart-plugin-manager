b747c7846faba1db9eebdd6d5a2051e21422f72c
diazoxide <aaron.yor@gmail.com>
2020-04-08 12:43:17 +0400

    Main plugin file

---
+++ smart-plugin-manager.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: WordPress Smart Plugin Manager
+ * Plugin URI:
+ * Description:
+ * Version: 1.0.1
+ * Author: Novembit
+ * Author URI:
+ * License: GPLv3
+ * Text Domain: novembit-spm
+ */
+
+use NovemBit\wp\plugins\spm\Bootstrap;
+
+defined('ABSPATH') || exit;
+
+include_once __DIR__ . '/vendor/autoload.php';
+
+Bootstrap::instance(__FILE__);

053ba0918406edb9fff6e195c130427c5733be4a
diazoxide <aaron.yor@gmail.com>
2020-04-09 12:08:15 +0400

    Deactivate and activate plugin

--- src/plugins/Plugins.php
+++ src/plugins/Plugins.php
@@ -23,6 +23,8 @@ class Plugins
     public const STATUS_FORCE_ENABLED = 1;
     public const STATUS_SMART = 2;
 
+    public const ACTION_PLUGIN_ACTIVATE = 'plugin_activate';
+
     /**
      * @return string
      */
@@ -40,9 +42,13 @@ class Plugins
         $this->parent = $parent;
 
         include_once ABSPATH . 'wp-admin/includes/plugin.php';
-        $this->plugins = get_plugins();
+
+        $this->setAllPlugins();
+
+        add_action('init', [$this, 'handleRequestActions']);
 
         $this->settings = [];
+
         $plugins = $this->getPluginsMap();
 
         foreach ($plugins as $file => $name) {
@@ -88,6 +94,33 @@ class Plugins
         }
     }
 
+    public function handleRequestActions(): void
+    {
+        if (isset($_GET[$this->getName()])
+            && wp_verify_nonce($_GET[$this->getName()], self::ACTION_PLUGIN_ACTIVATE)
+        ) {
+            $plugin = $_GET['plugin'] ?? null;
+            if (!$plugin) {
+                return;
+            }
+            $plugin = base64_decode($plugin);
+            $is_active = $this->plugins[$plugin]['custom_data']['is_active'] ?? false;
+
+            $is_active ? deactivate_plugins($plugin) : activate_plugins($plugin);
+
+            wp_redirect(wp_get_referer());
+        }
+    }
+
+    private function setAllPlugins(): void
+    {
+        $this->plugins = get_plugins();
+
+        foreach ($this->plugins as $plugin => &$data) {
+            $data['custom_data']['is_active'] = is_plugin_active($plugin);
+        }
+    }
+
     /**
      * @param $label
      * @param $route
@@ -124,18 +157,54 @@ class Plugins
         return $content;
     }
 
-    private function getPluginActions(string $plugin):string{
-        $html = '<div class="row actions">';
-        $html .= '<a class="button button-primary"></a>';
-        $html .= $this->getPluginActiveStatusBadge($plugin);
-        $html .='</div>';
-        return $html;
+    private function getPluginActions(string $plugin): string
+    {
+        global $wp;
+
+
+        $current_url = add_query_arg($wp->query_vars, admin_url($wp->request));
+        $current_url = add_query_arg(['plugin' => base64_encode($plugin)], $current_url);
+
+        $is_active = $this->plugins[$plugin]['custom_data']['is_active'] ?? false;
+
+        $label = $is_active ? __('Deactivate', 'novembit-spm') : __('Activate', 'novembit-spm');
+
+        $activate_url = wp_nonce_url($current_url, self::ACTION_PLUGIN_ACTIVATE, $this->getName());
+
+        ob_start();
+        ?>
+
+        <div class="plugin-actions">
+
+            <a href="<?php echo $activate_url; ?>" class="button button-default">
+                <?php echo $this->getPluginActiveStatusBadge($plugin,false); ?>
+                <?php echo $label; ?>
+            </a>
+
+        </div>
+
+        <?php
+        return ob_get_clean();
     }
 
+    /**
+     * @param string $plugin
+     * @return bool
+     */
+    private function isPluginActive(string $plugin): bool
+    {
+        return $this->plugins[$plugin]['custom_data']['is_active'] ?? false;
+    }
+
+    /**
+     * @param string $plugin
+     * @param bool $with_label
+     * @return string
+     */
     private function getPluginActiveStatusBadge(string $plugin, bool $with_label = true): string
     {
         $class = 'plugin-active-status-badge';
-        if (is_plugin_active($plugin)) {
+        if ($this->isPluginActive($plugin)) {
             $label = __('Activated', 'wordpress');
             $class .= ' activated';
         } else {
@@ -151,6 +220,8 @@ class Plugins
      */
     private function getPluginInfo(array $plugin_data): string
     {
+        unset($plugin_data['custom_data']);
+
         $table = '<table class="widefat fixed">';
         $i = 0;
         foreach ($plugin_data as $key => $value) {

a5c00147713e6a2448bf27f8ec155284c268997d
diazoxide <aaron.yor@gmail.com>
2020-04-14 11:03:29 +0400

    Fix Nardivan issue

--- smart-plugin-manager.php
+++ smart-plugin-manager.php
@@ -16,4 +16,4 @@ defined('ABSPATH') || exit;
 
 include_once __DIR__ . '/vendor/autoload.php';
 
-Bootstrap::instance(__FILE__);
+Bootstrap::instance(WP_PLUGIN_DIR . '/smart-plugin-manager/smart-plugin-manager.php');

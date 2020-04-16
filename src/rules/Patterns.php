4cb2b609aaa4a98ec1e1f70ae178861ead213b59
diazoxide <aaron.yor@gmail.com>
2020-04-16 18:20:53 +0400

    Add Auto tracking vars functionality

--- src/rules/Patterns.php
+++ src/rules/Patterns.php
@@ -5,11 +5,13 @@ namespace NovemBit\wp\plugins\spm\rules;
 
 
 use diazoxide\helpers\Arrays;
+use diazoxide\helpers\Environment;
+use diazoxide\helpers\HTML;
+use diazoxide\helpers\Variables;
 use diazoxide\wp\lib\option\v2\Option;
 
 class Patterns
 {
-
     /**
      * @var Rules
      * */
@@ -69,6 +71,12 @@ class Patterns
             [$this, 'predefinedPatterns']
         );
 
+
+        if (($this->parent->config['common']['auto_pattern_generation_tracker'] ?? false)) {
+            add_action('shutdown', [$this, 'initAutoPatternGenerationTracker']);
+        }
+
+
         $this->config = Option::expandOptions($this->settings, $this->getName());
 
         if (is_admin()) {
@@ -76,46 +84,55 @@ class Patterns
         }
     }
 
+    /**
+     * @param array $patterns
+     * @return array
+     */
     public function predefinedPatterns(array $patterns): array
     {
         $predefined_patterns = [
+            // Is Backend
             [
-                'name' => 'is_admin',
-                'label' => 'Is Administrator',
+                'name' => 'backend',
+                'label' => 'Backend',
                 'rules' =>
                     [
                         [
                             'rule' =>
                                 [
                                     [
-                                        'type' => 'function',
+                                        'type' => $this->parent::TYPE_FUNCTION,
                                         'key' => 'is_admin',
-                                        'compare' => 'equal',
+                                        'compare' => Variables::COMPARE_EQUAL,
                                         'value' => '1',
-                                        'logic' => 'and',
+                                        'logic' => $this->parent::LOGIC_AND,
                                     ],
                                 ],
-                            'logic' => 'and',
+                            'logic' => $this->parent::LOGIC_AND,
                         ],
                     ],
             ],
+            // Is frontend
             [
-                'name' => 'is_front',
-                'label' => 'Is Frontend',
+                'name' => 'frontend',
+                'label' => 'Frontend',
                 'rules' =>
                     [
                         [
                             'rule' =>
                                 [
                                     [
-                                        'type' => 'function',
-                                        'key' => 'is_admin',
-                                        'compare' => 'equal',
+                                        'type' => $this->parent::TYPE_FUNCTION,
+                                        'key' => 'defined',
+                                        'compare' => Variables::COMPARE_EQUAL,
                                         'value' => '0',
-                                        'logic' => 'and',
+                                        'params' => [
+                                            'WP_ADMIN'
+                                        ],
+                                        'logic' => $this->parent::LOGIC_AND,
                                     ],
                                 ],
-                            'logic' => 'and',
+                            'logic' => $this->parent::LOGIC_AND,
                         ],
                     ],
             ]
@@ -138,9 +155,14 @@ class Patterns
                 }
             }
         }
+
         return $patterns;
     }
 
+    /**
+     * @param array $patterns
+     * @return bool
+     */
     public function checkPatterns(array $patterns): bool
     {
         foreach ($patterns as $pattern) {
@@ -155,6 +177,10 @@ class Patterns
         return false;
     }
 
+    /**
+     * @param $name
+     * @return mixed|null
+     */
     public function getPattern($name)
     {
         return Arrays::ufind(
@@ -168,11 +194,17 @@ class Patterns
         );
     }
 
+    /**
+     * @return array
+     */
     public function getPatterns(): array
     {
         return $this->config['patterns'] ?? [];
     }
 
+    /**
+     * @return array
+     */
     public function getPatternsMap(): array
     {
         $list = [];
@@ -207,11 +239,89 @@ class Patterns
      */
     public function adminInit(): void
     {
+        add_action('admin_menu', [$this, 'adminMenu'], 11);
+    }
+
+    /**
+     * @var void
+     */
+    public function adminMenu(): void
+    {
         $this->parent->tabs['patterns'] = ['label' => 'Patterns', 'content' => [$this, 'tabContent']];
     }
 
-    public function tabContent()
+    /**
+     * @param $url
+     */
+    public function tabContent($url): void
     {
+        $this->initPatternsGenerator();
+
         Option::printForm($this->getName(), $this->settings);
+    }
+
+    private function initPatternsGenerator(): void
+    {
+        /** @var \WP_Rewrite $wp_rewrite */
+        global $wp_rewrite;
+
+        $extra = $wp_rewrite->extra_rules;
+        $extra_rules_top = $wp_rewrite->extra_rules_top;
+        $extra_param_struct = $wp_rewrite->extra_permastructs;
+
+
+        foreach ($extra_rules_top as $rule => $rewrite) {
+            echo $rule . '<br>';
+        }
+
+        return;
+
+
+        echo HTML::tagOpen('div', ['class' => 'wrap']);
+
+        echo HTML::tagOpen(
+            'form',
+            [
+                'action' => '',
+                'method' => 'post'
+            ]
+        );
+
+        echo HTML::tag('button', 'Generate', ['type' => 'submit', 'class' => 'button button-primary']);
+
+        wp_nonce_field('generate', $this->getName());
+
+        echo HTML::tagClose('form');
+        echo HTML::tagClose('div');
+    }
+
+    public function getTrackingVars(): array
+    {
+        return Option::getOption('tracking_vars', $this->getName(), []);
+    }
+
+    public function initAutoPatternGenerationTracker(): void
+    {
+        $tracking_vars = $this->getTrackingVars();
+
+        global $wp_rewrite;
+
+        /** @var \WP_Rewrite $wp_rewrite */
+        $tracking_vars['wp_rewrite']['extra_rules'] = array_merge(
+            $tracking_vars['wp_rewrite']['extra_rules'] ?? [],
+            $wp_rewrite->extra_rules
+        );
+
+        $tracking_vars['wp_rewrite']['extra_rules_top'] = array_merge(
+            $tracking_vars['wp_rewrite']['extra_rules_top'] ?? [],
+            $wp_rewrite->extra_rules_top
+        );
+
+        $tracking_vars['wp_rewrite']['extra_permastructs'] = array_merge(
+            $tracking_vars['wp_rewrite']['extra_permastructs'] ?? [],
+            $wp_rewrite->extra_permastructs
+        );
+
+        Option::setOption('tracking_vars', $this->getName(), $tracking_vars);
     }
 }
